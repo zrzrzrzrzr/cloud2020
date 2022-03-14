dbe83b66a50fb0101ff7ad4ea77d7ff76689d348
zhangrui <zhangrui>
2022-03-14 10:59:43 +0800

    second commit

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderFeignMain80 {
     public static void main(String[] args)
     {
